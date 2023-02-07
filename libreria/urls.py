6a016eba1f37281cdfef6f0174c873f4f52b5782
diulioz <diulioz.u@gmail.com>
2023-02-06 20:48:59 -0500

    2

--- libreria/urls.py
+++ libreria/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('inicio', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('libros', views.libros, name='libros'),
     path('crear', views.crear, name='crear'),
